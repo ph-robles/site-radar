5e3ef733cfe2e81104d5142331071a35bfc891bf
ph-robles <ph.robles33@gmail.com>
2026-03-08 23:02:43 -0300

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="border-t bg-white">
       <div className="container py-6 text-sm text-gray-600">
-        <p>© {new Date().getFullYear()} Site Radar — Feito com ♥ por Raphael Robles.@2026</p>
+        <p>© {new Date().getFullYear()} Site Radar — Desenvolvido por Raphael Robles ♥.</p>
       </div>
     </footer>
   );
