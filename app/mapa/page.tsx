2e6ed9a33904ace0f681e591d5af51cbbda57976
ph-robles <ph.robles33@gmail.com>
2026-04-02 23:11:39 -0300

    defynited

--- app/mapa/page.tsx
+++ app/mapa/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+// 🔥 ISSO É O QUE RESOLVE O ERRO DA VERCEL
+export const dynamic = "force-dynamic";
+
 import MapaLeaflet from "./MapaLeaflet";
 
 export default function MapaPage() {
