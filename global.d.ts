5013f1144da87a1d11f56e8cf81fb0025a09127e
ph-robles <raphaelrobles22@hotmail.com>
2026-04-02 22:15:24 -0300

    Update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-declare module "*.css";
-declare module "@mapbox/leaflet-omnivore";
-
-=======
 
 declare module "*.css";
 declare module "@mapbox/leaflet-omnivore";
 
 
-
->>>>>>> aff28af41c80b6e2906941080260b0f4d6c670a8
