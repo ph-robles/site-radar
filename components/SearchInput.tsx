c3648aa003f657f7af9542a7315b424c1a4dfc8e
ph-robles <192054274+ph-robles@users.noreply.github.com>
2026-08-24 10:25:30 -0300

    update searchInput

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -22,7 +22,7 @@ export default function SearchInput({ onSearch }: Props) {
       value={value}
       onChange={(e) => setValue(e.target.value)}
       onKeyDown={handleKeyDown}
-      className="w-full p-3 border rounded-xl"
+      className="w-full p-3 border rounded-xl text-purple-700 font-semibold"
     />
   );
 }
